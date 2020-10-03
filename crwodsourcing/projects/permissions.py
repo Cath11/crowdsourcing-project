a82f0484c90e8c87cee195d2cc4f21040dc5fb3f
Cath11 <Cath11@users.noreply.github.com>
2020-10-03 12:18:22 +0800

    cc

--- crwodsourcing/projects/permissions.py
+++ crwodsourcing/projects/permissions.py
@@ -5,12 +5,12 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-            return obj.owner == request.user
+        return obj.owner == request.user
 
 class IsNotOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-            return obj.project.owner != request.user
+        return obj.project.owner != request.user
 
 
