34bb9124d445d59040790032d0fe4390626bac41
Cath11 <Cath11@users.noreply.github.com>
2020-10-11 04:45:37 +0800

    git

--- crwodsourcing/users/admin.py
+++ crwodsourcing/users/admin.py
@@ -1,14 +1,12 @@
-from django.contrib import admin 
+from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
-
-from . models import CustomUser
-
 
 
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
     pass
-    
+
+
 admin.site.register(get_user_model(), CustomUserAdmin)
