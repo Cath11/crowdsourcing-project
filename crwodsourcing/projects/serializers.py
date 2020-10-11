83c9005afcc12bb91b6f9b4d4a7e550156e962d1
Cath11 <Cath11@users.noreply.github.com>
2020-10-11 11:42:07 +0800

    x

--- crwodsourcing/projects/serializers.py
+++ crwodsourcing/projects/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Project, Pledge
 
-        
+
 class ProjectSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
     title = serializers.CharField(max_length=200)
@@ -15,6 +15,7 @@ class ProjectSerializer(serializers.Serializer):
     def create(self, validated_data):
         return Project.objects.create(**validated_data)
 
+
 class PledgeSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
     amount = serializers.IntegerField()
@@ -22,7 +23,7 @@ class PledgeSerializer(serializers.Serializer):
     anonymous = serializers.BooleanField()
     supporter = serializers.CharField(max_length=200)
     project_id = serializers.IntegerField()
-    
+
     def create(self, validated_data):
         return Pledge.objects.create(**validated_data)
 
@@ -32,14 +33,17 @@ class ProjectDetailSerializer(ProjectSerializer):
 
     def update(self, instance, validated_data):
         instance.title = validated_data.get('title', instance.title)
-        instance.description = validated_data.get('description', instance.description)
+        instance.description = validated_data.get('description',
+                                                  instance.description)
         instance.goal = validated_data.get('goal', instance.goal)
         instance.image = validated_data.get('image', instance.image)
         instance.is_open = validated_data.get('is_open', instance.is_open)
-        instance.date_created = validated_data.get('date_created', instance.date_created)
+        instance.date_created = validated_data.get('date_created',
+                                                   instance.date_created)
         instance.owner = validated_data.get('owner', instance.owner)
         instance.save()
         return instance
+
 
 class PledgeDetailSerializer(PledgeSerializer):
     pledges = PledgeSerializer(many=True, read_only=True)
@@ -47,17 +51,19 @@ class PledgeDetailSerializer(PledgeSerializer):
     def update(self, instance, validated_data):
         instance.amount = validated_data.get('amount', instance.amount)
         instance.comment = validated_data.get('comment', instance.comment)
-        instance.anonymous = validated_data.get('anonymous', instance.anonymous)
-        instance.supporter = validated_data.get('supporter', instance.supporter)
-        instance.project_id = validated_data.get('project_id', instance.project_id)
+        instance.anonymous = validated_data.get('anonymous',
+                                                instance.anonymous)
+        instance.supporter = validated_data.get('supporter',
+                                                instance.supporter)
+        instance.project_id = validated_data.get('project_id',
+                                                 instance.project_id)
         instance.save()
         return instance
-        
+
+
 # class ProjectCategorySerializer(serializers.Serializer):
 #     id = serializers.ReadOnlyField()
 #     name = serializers.CharField(max_length=50)
 
 #     def create(self, validated_data):
 #         return ProjectCategory.objects.create(**validated_data)
-
-   
